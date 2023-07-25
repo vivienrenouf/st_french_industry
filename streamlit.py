4dc1a6a9e1a2b3361d47824338ec2f35c59174a3
vivienrenouf <vivienrenouf@gmail.com>
2023-07-25 22:25:06 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("""Modélisation : indicateur de pauvreté""")
+st.title("""Pauvreté en France \nModélisation d'un indicateur""")
 st.image('pauvrete.jpeg')
 st.write("""La variable TP6020 est une variable publiée par l’INSEE correspondant au taux de pauvreté en 2020. Ce taux est calculé pour les personnes logées de manière ordinaire en France métropolitaine. Il exclut donc les sans-abris et les populations occupant des habitations mobiles. Les ménages dont la personne de référence est étudiante sont aussi exclus de l’analyse. Ce taux est calculé par l’INSEE à partir de l’enquête Revenus fiscaux et sociaux (ERFS), réalisée annuellement.
 """)
