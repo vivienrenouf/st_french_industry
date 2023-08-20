e4f0b968d3cfb3f4952879daf1b2f83510b63beb
Vivien Renouf <vivienrenouf@gmail.com>
2023-08-20 15:25:50 +0200

    folium

--- streamlit.py
+++ streamlit.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from streamlit_folium import st_folium
 
 
 
@@ -11,8 +12,17 @@ with st.sidebar:
 
 
 st.title("""Indicateur de pauvreté""")
-st.image('pauvrete.jpeg')
-st.write("""La variable TP6020 est une variable publiée par l’INSEE correspondant au taux de pauvreté en 2020. Ce taux est calculé pour les personnes logées de manière ordinaire en France métropolitaine. Il exclut donc les sans-abris et les populations occupant des habitations mobiles. Les ménages dont la personne de référence est étudiante sont aussi exclus de l’analyse. Ce taux est calculé par l’INSEE à partir de l’enquête Revenus fiscaux et sociaux (ERFS), réalisée annuellement.
-    """)
+#st.image('pauvrete.jpeg')
+#st.write("""La variable TP6020 est une variable publiée par l’INSEE correspondant au taux de pauvreté en 2020. Ce taux est calculé pour les personnes logées de manière ordinaire en France métropolitaine. Il exclut donc les sans-abris et les populations occupant des habitations mobiles. Les ménages dont la personne de référence est étudiante sont aussi exclus de l’analyse. Ce taux est calculé par l’INSEE à partir de l’enquête Revenus fiscaux et sociaux (ERFS), réalisée annuellement.""")
+
+
+# center on Liberty Bell, add marker
+m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
+folium.Marker(
+    [39.949610, -75.150282], popup="Liberty Bell", tooltip="Liberty Bell"
+).add_to(m)
+
+# call to render Folium map in Streamlit
+st_data = st_folium(m, width=725)
 
 
