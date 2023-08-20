2d9789deb29d45f23305ef7b634245565c5d7e7b
Vivien Renouf <vivienrenouf@gmail.com>
2023-08-20 19:42:58 +0200

    lille map

--- streamlit.py
+++ streamlit.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import folium
 from streamlit_folium import st_folium
+import pandas as pd
 
 
 
@@ -16,12 +17,19 @@ st.title("""Indicateur de pauvreté""")
 #st.image('pauvrete.jpeg')
 #st.write("""La variable TP6020 est une variable publiée par l’INSEE correspondant au taux de pauvreté en 2020. Ce taux est calculé pour les personnes logées de manière ordinaire en France métropolitaine. Il exclut donc les sans-abris et les populations occupant des habitations mobiles. Les ménages dont la personne de référence est étudiante sont aussi exclus de l’analyse. Ce taux est calculé par l’INSEE à partir de l’enquête Revenus fiscaux et sociaux (ERFS), réalisée annuellement.""")
 
+df = pd.read_csv('..\files\lille_folium.csv')
 
 # center on Liberty Bell, add marker
-m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
-folium.Marker(
-    [39.949610, -75.150282], popup="Liberty Bell", tooltip="Liberty Bell"
-).add_to(m)
+m = folium.Map(location=[50.62, 3.05], zoom_start=10, tiles="CartoDB positron")
+
+for _, r in df.iterrows():
+    # Without simplifying the representation of each borough,
+    # the map might not be displayed
+    sim_geo = geopandas.GeoSeries(r["geometry"]).simplify(tolerance=0.001)
+    geo_j = sim_geo.to_json()
+    geo_j = folium.GeoJson(data=geo_j, style_function=lambda x: {"fillColor": "orange"})
+    folium.Popup(r["LIBGEO"]).add_to(geo_j)
+    geo_j.add_to(m)
 
 # call to render Folium map in Streamlit
 st_data = st_folium(m, width=725)
