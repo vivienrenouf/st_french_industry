c45cfaefda4133ae857ae4d7d39a0e632173d079
Vivien Renouf <vivienrenouf@gmail.com>
2023-08-20 19:48:18 +0200

    deplacement csv

--- streamlit.py
+++ streamlit.py
@@ -17,7 +17,7 @@ st.title("""Indicateur de pauvreté""")
 #st.image('pauvrete.jpeg')
 #st.write("""La variable TP6020 est une variable publiée par l’INSEE correspondant au taux de pauvreté en 2020. Ce taux est calculé pour les personnes logées de manière ordinaire en France métropolitaine. Il exclut donc les sans-abris et les populations occupant des habitations mobiles. Les ménages dont la personne de référence est étudiante sont aussi exclus de l’analyse. Ce taux est calculé par l’INSEE à partir de l’enquête Revenus fiscaux et sociaux (ERFS), réalisée annuellement.""")
 
-df = pd.read_csv('../files/lille_folium.csv')
+df = pd.read_csv('lille_folium.csv')
 
 # center on Liberty Bell, add marker
 m = folium.Map(location=[50.62, 3.05], zoom_start=10, tiles="CartoDB positron")
