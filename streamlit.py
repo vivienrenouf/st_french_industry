d64077ff5e38d362ae5bd2b8db8a6904d131d3d8
Vivien Renouf <v.renouf@luneautech.com>
2023-08-20 13:23:32 +0200

    2nd slider

--- streamlit.py
+++ streamlit.py
@@ -7,4 +7,4 @@ st.write("""La variable TP6020 est une variable publiée par l’INSEE correspon
 
 
 chomage = st.slider('Ajustez le taux de chômage', 0, 100, 50)
-st.write('Le taux de chômage est de', chomage)
+ss_diplome = st.slider('Ajustez le taux de non diplômés', 0, 100, 50)
