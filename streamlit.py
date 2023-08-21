cea59e6d55bd6689392d42301d2fc2dd30d47b9e
Vivien Renouf <vivienrenouf@gmail.com>
2023-08-21 22:55:15 +0200

    json?

--- streamlit.py
+++ streamlit.py
@@ -3,6 +3,7 @@ import folium
 from streamlit_folium import st_folium
 import pandas as pd
 import geopandas
+import json
 
 st.set_page_config(layout="wide")
 
@@ -22,7 +23,7 @@ st.title("""Aire d'attraction de Lille : indicateur de pauvreté""")
 #st.image('pauvrete.jpeg')
 #st.write("""La variable TP6020 est une variable publiée par l’INSEE correspondant au taux de pauvreté en 2020. Ce taux est calculé pour les personnes logées de manière ordinaire en France métropolitaine. Il exclut donc les sans-abris et les populations occupant des habitations mobiles. Les ménages dont la personne de référence est étudiante sont aussi exclus de l’analyse. Ce taux est calculé par l’INSEE à partir de l’enquête Revenus fiscaux et sociaux (ERFS), réalisée annuellement.""")
 
-geo_data_lille = f"lille_folium.geojson"
+geo_data_lille = json.load(open('lille_folium.geojson'))
 
 
 m = folium.Map(location=[50.62, 3.05], zoom_start=10, tiles="CartoDB positron")
@@ -37,7 +38,7 @@ for _, r in df.iterrows():
 """
   
 folium.Choropleth(
-    geo_data="geo_data_lille" ,
+    geo_data=geo_data_lille ,
     name="choropleth",
     data=lille,
     columns=["LIBGEO", "TP6020"],
