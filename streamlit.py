a9d2442261a814350330116d95dd7c72f187c9f2
Vivien Renouf <vivienrenouf@gmail.com>
2023-08-21 19:18:21 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -31,7 +31,7 @@ m = folium.Map(location=[50.62, 3.05], zoom_start=10, tiles="CartoDB positron")
 for _, r in df.iterrows():
     sim_geo = geopandas.GeoSeries(r["geometry"]).simplify(tolerance=0.001)
     geo_j= sim_geo.to_json()
-    #geo_j = folium.GeoJson(data=geo_j, style_function=lambda x: {"fillColor": "orange"})
+    geo_j = folium.GeoJson(data=geo_j, style_function=lambda x: {"fillColor": "orange"})
     folium.Popup(r["LIBGEO"]).add_to(geo_j)
     geo_j.add_to(m)
 
