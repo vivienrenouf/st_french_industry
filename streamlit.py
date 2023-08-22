04be11d762589985ec0c312c0aee2c83afb0bfe8
Vivien Renouf <vivienrenouf@gmail.com>
2023-08-22 22:52:10 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -20,7 +20,7 @@ with st.sidebar:
     tx_location = st.slider('Ajustez le taux de locataires', 0, 100, 80)
 
 
-st.title("""Prédiction du taux de pauvreté de l'aire d'attraction de Lille""")
+st.title("""Taux de pauvreté de l'aire d'attraction de Lille""")
 #st.image('pauvrete.jpeg')
 #st.write("""La variable TP6020 est une variable publiée par l’INSEE correspondant au taux de pauvreté en 2020. Ce taux est calculé pour les personnes logées de manière ordinaire en France métropolitaine. Il exclut donc les sans-abris et les populations occupant des habitations mobiles. Les ménages dont la personne de référence est étudiante sont aussi exclus de l’analyse. Ce taux est calculé par l’INSEE à partir de l’enquête Revenus fiscaux et sociaux (ERFS), réalisée annuellement.""")
 
