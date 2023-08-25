f5af1ed8a44006f99fcc84ca8c0850183b7b0b16
vivienrenouf <vivienrenouf@gmail.com>
2023-08-25 23:11:24 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -22,11 +22,14 @@ with st.sidebar:
     st.sidebar.title("Instructions")
     select_commune = st.selectbox("Sélectionnez une commune de l'agglomération", (communes))
 
+    #Pour fonctionner correctement en fonction du dropdown menu, et même si les indicateurs ne sont pas dans le sidebar, ces variables doivent être stockées directement après le select_commune et en amont des jauges.
     tx_pauvrete_commune = lille.loc[lille['LIBGEO'] == select_commune, 'TP6020'].values[0]
     tx_pauvrete_france = 14.6
     delta_moyenne = tx_pauvrete_commune - tx_pauvrete_france
 
     tx_chomage_commune = lille.loc[lille['LIBGEO'] == select_commune, 'Taux_chomage'].values[0]
+
+    tx_ss_diplome_commune = lille.loc[lille['LIBGEO'] == select_commune, 'taux_sans_diplome'].values[0]
 
     st.write("""Le taux de pauvreté représente la part des ménages dont le revenu disponible est inférieur à 60% du niveau de vie médian national. 
     En France, ce taux est de 14,6%. Il s'agit d'un indicateur purement monétaire. \n\n Afin d'aider les agglomérations à lutter contre la pauvreté, 
@@ -50,6 +53,8 @@ with tab1:
         st.metric(label='Taux de pauvreté monétaire à '+ select_commune, value='{} %'.format(tx_pauvrete_commune), delta='{} %'.format(delta_moyenne), delta_color="inverse")
     with col2:
         st.metric(label='Taux de chômage à '+ select_commune, value='{} %'.format(tx_chomage_commune))
+    with col3:
+        st.metric(label='Taux de chômage à '+ select_commune, value='{} %'.format(tx_ss_diplome_commune))
 
     m = folium.Map(location=[50.62, 3.05], zoom_start=10, tiles="CartoDB positron")
 
