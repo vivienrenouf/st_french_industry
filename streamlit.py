2b65e114020cece2079595cec9a25655cfd3796a
vivienrenouf <vivienrenouf@gmail.com>
2023-08-25 23:57:38 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -39,7 +39,7 @@ with st.sidebar:
     cet outil effectue des prédictions du taux de pauvreté monétaire **à partir de données non monétaires**. 
     En effectuant des simulations axées sur l'emploi, la formation et le logement, les collectivités pourront observer l'impact que pourraient avoir leurs futures politiques contre
     les inégalités.\n\n Ajustez les taux ci-dessous et observez la prédiction du taux de pauvreté :""")
-    tx_chomage = st.slider('Ajustez le taux de chômage', 0, 100, int(tx_chomage_commune))
+    tx_chomage = st.slider('Ajustez le taux de chômage', 0.0, 100.0, tx_chomage_commune)
     tx_ss_diplome = st.slider('Ajustez le taux de non diplômés', 0, 100, int(tx_ss_diplome_commune))
     tx_inactifs = st.slider("Ajustez le taux d'inactifs", 0, 100, int(tx_inactifs_commune))
     tx_location = st.slider('Ajustez le taux de locataires', 0, 100, int(tx_location_commune))
