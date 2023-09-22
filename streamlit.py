7ff147edc77b61a98ea2ee709ce9612c9994bc2e
vivienrenouf <vivienrenouf@gmail.com>
2023-09-22 13:39:30 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -61,7 +61,7 @@ with st.sidebar:
     prediction = model.predict(inputs)
     rounded_prediction = np.round(prediction[0], 1)
     st.success('Prédiction : {} %'.format(rounded_prediction))
-    st.metric(label=green['Prédiction'], value='{} %'.format(rounded_prediction))
+    st.metric(label=:green['Prédiction'], value='{} %'.format(rounded_prediction))
 
     st.divider()
     st.caption(""" \* L'INSEE ne publie qu'une partie des données communales (secret statistique). 
