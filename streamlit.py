c101b65f0e6ffcffdd8d44e037baf4d3ea7e89d0
vivienrenouf <vivienrenouf@gmail.com>
2023-09-22 09:18:07 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -26,8 +26,8 @@ with st.sidebar:
     st.sidebar.title("Paramètres")
 
 
-    st.write("Sélectionnez une commune de l'aire d'attraction de Lille")
-    select_commune = st.selectbox('', (communes),  index=43) #l'index 43 correspond à la commune de Lille. Affichage par défaut.
+    st.write(")
+    select_commune = st.selectbox('Sélectionnez une commune de l'aire d'attraction de Lille"', (communes),  index=43) #l'index 43 correspond à la commune de Lille. Affichage par défaut.
 
     #Pour fonctionner correctement en fonction du dropdown menu, et même si les indicateurs ne sont pas dans le sidebar, ces variables doivent être stockées directement après le select_commune et en amont des jauges.
     tx_pauvrete_commune = lille.loc[lille['LIBGEO'] == select_commune, 'TP6020'].values[0]
