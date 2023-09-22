f1acb15309f093d39b01747219a0cac68651df2d
vivienrenouf <vivienrenouf@gmail.com>
2023-09-22 09:02:44 +0200

    change titre sidebar

--- streamlit.py
+++ streamlit.py
@@ -23,7 +23,7 @@ with open(pickle_filename, 'rb') as file:
 
 
 with st.sidebar:
-    st.sidebar.title("Instructions")
+    st.sidebar.title("Paramètres")
     select_commune = st.selectbox("Sélectionnez une commune de l'aire d'attraction de Lille", (communes),  index=43) #l'index 43 correspond à la commune de Lille. Affichage par défaut.
 
     #Pour fonctionner correctement en fonction du dropdown menu, et même si les indicateurs ne sont pas dans le sidebar, ces variables doivent être stockées directement après le select_commune et en amont des jauges.
@@ -66,7 +66,7 @@ with st.sidebar:
 
 st.title("Analyse de la pauvreté à " + select_commune)
 
-tab1, tab2 = st.tabs(['Cartographie', 'TBD'])
+tab1, tab2 = st.tabs(['Cartographie', 'Instructions'])
 
 #Premier onglet
 with tab1:
