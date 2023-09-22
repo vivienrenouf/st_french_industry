114cb5334bf415e11a5cc1339a57e9a9b4dbfeb2
vivienrenouf <vivienrenouf@gmail.com>
2023-09-22 18:14:05 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -17,11 +17,6 @@ with open(pickle_filename, 'rb') as file:
     model = pickle.load(file)
 
 
-
-#st.image('pauvrete.jpeg')
-#st.write("""La variable TP6020 est une variable publiée par l’INSEE correspondant au taux de pauvreté en 2020. Ce taux est calculé pour les personnes logées de manière ordinaire en France métropolitaine. Il exclut donc les sans-abris et les populations occupant des habitations mobiles. Les ménages dont la personne de référence est étudiante sont aussi exclus de l’analyse. Ce taux est calculé par l’INSEE à partir de l’enquête Revenus fiscaux et sociaux (ERFS), réalisée annuellement.""")
-
-
 with st.sidebar:
     st.sidebar.title("Paramètres")
 
