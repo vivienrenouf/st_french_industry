191d60b15f15bb827576cfe7e9c5497f3f169472
vivienrenouf <vivienrenouf@gmail.com>
2023-09-24 00:07:35 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -48,7 +48,13 @@ with st.sidebar:
     instructions = '<p style="font-size: 18px;"><strong>Ajustez les jauges et observez la prédiction !</strong></p>'
     st.markdown(instructions, unsafe_allow_html=True)
 
-    tx_chomage = st.slider('Taux de chômage', 0.0, 100.0, tx_chomage_commune, step=0.1, format="%f")
+    st.container()
+    jauge, button = st.columns(2)
+    with jauge:
+        tx_chomage = st.slider('Taux de chômage', 0.0, 100.0, tx_chomage_commune, step=0.1, format="%f")
+    with button:
+        st.button(label='reset', on_click=tx_chomage = st.slider('Taux de chômage', 0.0, 100.0, tx_chomage_commune, step=0.1, format="%f"))
+
     tx_ss_diplome = st.slider('Taux de non diplômés', 0.0, 100.0, tx_ss_diplome_commune, step=0.1, format="%f")
     tx_inactifs = st.slider("Taux d'inactifs", 0.0, 100.0, tx_inactifs_commune, step=0.1, format="%f")
     tx_location = st.slider('Taux de locataires (hab. princ.)', 0.0, 100.0, tx_location_commune, step=0.1, format="%f")
